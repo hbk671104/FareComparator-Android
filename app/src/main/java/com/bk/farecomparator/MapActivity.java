811414a270c7448d0844a9aad7bcc9ff6d2085cb
hbk671104 <hbk671104@gmail.com>
2016-10-22 17:35:22 +0800

    user location

--- app/src/main/java/com/bk/farecomparator/MapActivity.java
+++ app/src/main/java/com/bk/farecomparator/MapActivity.java
@@ -2,9 +2,17 @@ package com.bk.farecomparator;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
+import com.amap.api.location.AMapLocation;
+import com.amap.api.location.AMapLocationClient;
+import com.amap.api.location.AMapLocationClientOption;
+import com.amap.api.location.AMapLocationListener;
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
 import com.amap.api.maps.LocationSource;
 import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.LatLng;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -13,6 +21,8 @@ public class MapActivity extends AppCompatActivity {
 
     @BindView(R.id.main_map)
     MapView mainMap;
+    private AMapLocationClient aMapLocationClient;
+    private LocationSource.OnLocationChangedListener locationChangedListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,15 +40,47 @@ public class MapActivity extends AppCompatActivity {
             @Override
             public void activate(OnLocationChangedListener onLocationChangedListener) {
                 // TODO: integrate location sdk
+                locationChangedListener = onLocationChangedListener;
+                aMapLocationClient.startLocation();
             }
 
             @Override
             public void deactivate() {
-
+                locationChangedListener = null;
+                if (aMapLocationClient != null) {
+                    aMapLocationClient.stopLocation();
+                    aMapLocationClient.onDestroy();
+                }
+                aMapLocationClient = null;
             }
         });
         mainMap.getMap().getUiSettings().setMyLocationButtonEnabled(true);
         mainMap.getMap().setMyLocationEnabled(true);
+        mainMap.getMap().setMyLocationType(AMap.LOCATION_TYPE_LOCATE);
+
+        // Amap location client init
+        aMapLocationClient = new AMapLocationClient(this);
+        aMapLocationClient.setLocationListener(new AMapLocationListener() {
+            @Override
+            public void onLocationChanged(AMapLocation aMapLocation) {
+                if (locationChangedListener != null && aMapLocation != null) {
+                    if (aMapLocation.getErrorCode() == 0) {
+                        locationChangedListener.onLocationChanged(aMapLocation);
+                        mainMap.getMap().animateCamera(CameraUpdateFactory.newLatLngZoom(
+                                new LatLng(
+                                        aMapLocation.getLatitude(),
+                                        aMapLocation.getLongitude()),
+                                15));
+                    } else {
+                        String errText = aMapLocation.getErrorCode() + ": " + aMapLocation.getErrorInfo();
+                        Toast.makeText(MapActivity.this, errText, Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+        AMapLocationClientOption option = new AMapLocationClientOption();
+        option.setLocationMode(AMapLocationClientOption.AMapLocationMode.Hight_Accuracy);
+        aMapLocationClient.setLocationOption(option);
     }
 
     @Override
